dcb190cbc41f4e8e4410144b1acae5574ba2505d
OrdinaryEnder <zairullahfreefire@outlook.com>
2025-01-10 19:17:38 +0700

    camellia: Add unofficial support for Derpfest/Arrow_Extended

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,18 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_camellia.mk
+    $(LOCAL_DIR)/aosp_camellia.mk \
+    $(LOCAL_DIR)/lineage_camellia.mk \
+    $(LOCAL_DIR)/arrow_camellia.mk \
+    $(LOCAL_DIR)/derp_camellia.mk \
 
 COMMON_LUNCH_CHOICES := \
-    aosp_camellia-user \
-    aosp_camellia-userdebug \
-    aosp_camellia-eng
+    lineage_camellia-user \
+    lineage_camellia-userdebug \
+    lineage_camellia-eng \
+    arrow_camellia-user \
+    arrow_camellia-userdebug \
+    arrow_camellia-eng \
+    derp_camellia-user \
+    derp_camellia-userdebug \
+    derp_camellia-eng \
